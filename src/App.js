699128486ddbcaaabf3702f739aac93c8869338e
Tony Wang <wangh9947@gmail.com>
2023-09-02 17:00:27 +0800

    p3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from "./logo.svg";
 import "./App.css";
+import _ from "lodash";
+import { useState } from "react";
 
 function App() {
   const initialTasks = [
-    { text: "play football", color: "black", completed: true },
-    { text: "play game", color: "red", completed: false },
-    { text: "shut window", color: "purple", completed: false },
+    { text: "play football", color: "black", isCompleted: true },
+    { text: "play game", color: "red", isCompleted: false },
+    { text: "shut window", color: "purple", isCompleted: false },
   ];
+  const [tasks, setTasks] = useState(initialTasks);
+  const [userInput, setUserInput] = useState("");
   return (
     <div className="App">
       <h1>To Dos</h1>
-      <input></input>
-      <button>Add Task</button>
+      <input
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+        value={userInput}
+      ></input>
+      <button
+        onClick={() => {
+          const copyTasks = _.cloneDeep(tasks);
+          copyTasks.push({
+            text: userInput,
+            color: "black",
+            isCompleted: false,
+          });
 
-      {initialTasks.map((e, i) => {
+          setTasks(copyTasks);
+          console.log(tasks);
+          setUserInput("");
+        }}
+      >
+        Add Task
+      </button>
+      <button
+        onClick={() => {
+          console.log(tasks);
+        }}
+      >
+        show
+      </button>
+
+      {tasks.map((e, i) => {
         return (
           <div key={i} style={{ color: e.color }}>
             {e.text}
-            <input type="checkbox" />
+            <input
+              onChange={() => {
+                const copy = _.cloneDeep(tasks);
+                copy[i].isCompleted = !copy[i].isCompleted;
+                setTasks(copy);
+              }}
+              checked={e.isCompleted}
+              type="checkbox"
+            />
+            <button
+              onClick={() => {
+                const copy = _.cloneDeep(tasks);
+                const newTask = copy.slice(0, i).concat(copy.slice(i + 1));
+                setTasks(newTask);
+              }}
+            >
+              -
+            </button>
           </div>
         );
       })}
